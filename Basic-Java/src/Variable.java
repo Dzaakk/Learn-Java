316d936df01b4c251b4d90ffe716f29c6ad1af56
Dzkzzz <dzdzaakk@gmail.com>
2024-01-04 19:34:15 +0700

    variable

---
+++ Basic-Java/src/Variable.java
@@ -0,0 +1,24 @@
+public class Variable {
+    public static void main(String[] args) {
+        String name;
+        name = "yato";
+
+        System.out.println(name);
+
+        int age = 10;
+        String address = "Indonesia";
+
+        System.out.println(age);
+        System.out.println(address);
+
+        name = "Bishamon";
+
+        System.out.println(name);
+
+        //langsung isi valuenya
+        var name2 = "Kinemon";
+
+        //Constant
+        final String application = "Java";
+    }
+}
