6d4982996a1a4718401afa606fb794eed318138e
Dzkzzz <dzdzaakk@gmail.com>
2024-01-04 19:19:04 +0700

    tipe data boolean

---
+++ Basic-Java/src/TipeDataBoolean.java
@@ -0,0 +1,9 @@
+public class TipeDataBoolean {
+    public static void main(String[] args) {
+        boolean b = true;
+        boolean s = false;
+
+        System.out.println(b);
+        System.out.println(s);
+    }
+}
