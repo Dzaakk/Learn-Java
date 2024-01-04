4473fdbae00a477f5e8d591e9a4037be1f48a6b1
Dzkzzz <dzdzaakk@gmail.com>
2024-01-04 19:16:01 +0700

    tipe data char

---
+++ Basic-Java/src/TipeDataCharacter.java
@@ -0,0 +1,11 @@
+public class TipeDataCharacter {
+    public static void main(String[] args) {
+        char e = 'E';
+        char f = 'F';
+        char g = 'G';
+
+        System.out.println(e);
+        System.out.println(f);
+        System.out.println(g);
+    }
+}
