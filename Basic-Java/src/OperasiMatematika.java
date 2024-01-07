00e5f8779b7ec9fc69db693597e8ec63cdc867a2
Dzkzzz <dzdzaakk@gmail.com>
2024-01-07 18:23:07 +0700

    operasi matematika

---
+++ Basic-Java/src/OperasiMatematika.java
@@ -0,0 +1,25 @@
+public class OperasiMatematika {
+    public static void main(String[] args) {
+        int a = 100;
+        int b = 9;
+
+        System.out.println(a + b);
+        System.out.println(a - b);
+        System.out.println(a * b);
+        System.out.println(a / b);
+        System.out.println(a % b);
+
+        //Augmented Assignment
+        int c = 100;
+
+        c += 10;
+        System.out.println(c);
+
+        c -= 10;
+        System.out.println(c);
+
+        c %= 10;
+        System.out.println(c);
+
+    }
+}
