ec374a1ecd109f498f4e72a5c085c73d1ac996f5
Dzkzzz <dzdzaakk@gmail.com>
2024-01-07 18:39:09 +0700

    expression, statement, block

---
+++ Basic-Java/src/Block.java
@@ -0,0 +1,23 @@
+import java.util.Date;
+
+public class Block {
+    //Block *from { to }
+    public static void main(String[] args) {
+        //expression
+        int value;
+        value = 10;
+
+        //statement *semua baris code sampai dengan titik koma
+        System.out.println(value = 100);
+
+        //assignment statement
+        double aValue = 8933.234;
+        //increment statement
+        aValue++;
+        //method invocation statement
+        System.out.println("Hello!");
+        //object creation statement
+        Date date = new Date();
+
+    }
+}
