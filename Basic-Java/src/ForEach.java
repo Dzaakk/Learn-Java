42c9f45aafe473215b54a7b26c2e50069a8e32f1
Dzkzzz <dzdzaakk@gmail.com>
2024-01-09 22:06:32 +0700

    for each

---
+++ Basic-Java/src/ForEach.java
@@ -0,0 +1,13 @@
+public class ForEach {
+    public static void main(String[] args) {
+
+        String[] arrayData = {
+                "Data 1","Data 2",
+                "Data 3","Data 4","Data 5"
+        };
+
+        for (var Data: arrayData){
+            System.out.println(Data);
+        }
+    }
+}
