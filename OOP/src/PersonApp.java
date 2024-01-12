d8b1cd27f59100a5c209378bb9e3ece58d5fcc84
Dzkzzz <dzdzaakk@gmail.com>
2024-01-12 23:27:32 +0700

    method

--- OOP/src/PersonApp.java
+++ OOP/src/PersonApp.java
@@ -8,9 +8,13 @@ public class PersonApp {
         System.out.println(person1.address);
         System.out.println(person1.country);
 
+        person1.sayHello("Bambang");
+
         Person person2 = new Person();
+
         Person person3;
         person3 = new Person();
+        person3.sayHello("Tono");
 
     }
 }
