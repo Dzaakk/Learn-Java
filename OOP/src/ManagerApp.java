1b476615e253675048b29607bcf0533d5b7a1227
Dzkzzz <dzdzaakk@gmail.com>
2024-01-16 22:25:46 +0700

    super constructor

--- OOP/src/ManagerApp.java
+++ OOP/src/ManagerApp.java
@@ -1,11 +1,9 @@
 public class ManagerApp {
     public static void main(String[] args) {
-        var manager = new Manager();
-        manager.name = "Xizi";
+        var manager = new Manager("Xizi");
         manager.sayHello("Ron");
 
-        var vp = new VicePresident();
-        vp.name = "alex";
+        var vp = new VicePresident("Alex");
         vp.sayHello("Joe");
     }
 }
