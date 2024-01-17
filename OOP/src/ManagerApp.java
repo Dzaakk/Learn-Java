77e8eeb5a20852eb363749b2a0747d51bc0de6a0
Dzkzzz <dzdzaakk@gmail.com>
2024-01-17 21:34:01 +0700

    object class

--- OOP/src/ManagerApp.java
+++ OOP/src/ManagerApp.java
@@ -5,5 +5,10 @@ public class ManagerApp {
 
         var vp = new VicePresident("Alex");
         vp.sayHello("Joe");
+
+        System.out.println(manager);
+        System.out.println(manager.toString());
+        System.out.println(vp);
+        System.out.println(vp.toString());
     }
 }
