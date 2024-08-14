00663eb8dfaec22e22bac55c3b0cec59c5bb3da1
Dzkzzz <dzdzaakk@gmail.com>
2024-08-14 16:09:26 +0700

    access modifier

---
+++ OOP/src/learn/data/ProductApp.java
@@ -0,0 +1,10 @@
+package learn.data;
+
+public class ProductApp {
+    public static void main(String[] args) {
+        Product product = new Product("Mac book Pro", 30000000);
+
+        System.out.println(product.name);
+        System.out.println(product.price);
+    }
+}
