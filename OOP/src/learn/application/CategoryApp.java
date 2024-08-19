956bf199f528b3db4cbaa5a49d40ac9d91615c49
Dzkzzz <dzdzaakk@gmail.com>
2024-08-19 22:17:35 +0700

    getter and setter

---
+++ OOP/src/learn/application/CategoryApp.java
@@ -0,0 +1,13 @@
+package learn.application;
+
+import learn.data.Category;
+
+public class CategoryApp {
+    public static void main(String[] args) {
+        var category = new Category();
+        category.setId("ID");
+        category.setId(null);
+
+        System.out.println(category.getId());
+    }
+}
