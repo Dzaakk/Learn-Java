589786fe7492e157ad7e0e944ed5a4e345d893c4
Dzkzzz <dzdzaakk@gmail.com>
2024-08-21 22:03:38 +0700

    big number class

---
+++ Java-Standard-Classes/BigNumberApp.java
@@ -0,0 +1,11 @@
+import java.math.BigInteger;
+
+public class BigNumberApp {
+    public static void main(String[] args) {
+        BigInteger a = new BigInteger("1000000000000000000000000");
+        BigInteger b = new BigInteger("2000000000000000000000000");
+
+        BigInteger c = a.add(b);
+        System.out.println(c);
+    }
+}
