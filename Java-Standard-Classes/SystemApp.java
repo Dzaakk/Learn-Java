b2e3cd985b7531c9eb21e233191b620b779d8ca0
Dzkzzz <dzdzaakk@gmail.com>
2024-08-21 22:20:01 +0700

    system class

---
+++ Java-Standard-Classes/SystemApp.java
@@ -0,0 +1,11 @@
+public class SystemApp {
+    public static void main(String[] args) {
+        System.out.println(System.currentTimeMillis());
+        System.out.println(System.nanoTime());
+
+        System.out.println(System.getenv("HOME"));
+
+        System.exit(0);
+        System.out.println("Hi");
+    }
+}
