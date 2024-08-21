442c65f5544c7f038a6fb0c812ea9419fcec370d
Dzkzzz <dzdzaakk@gmail.com>
2024-08-21 22:00:44 +0700

    math class

---
+++ Java-Standard-Classes/MathApp.java
@@ -0,0 +1,12 @@
+public class MathApp {
+    public static void main(String[] args) {
+        var min = Math.min(1000, 3000);
+        System.out.println(min);
+
+        var max = Math.max(1000, 3000);
+        System.out.println(max);
+
+        System.out.println(Math.PI);
+        System.out.println(Math.E);
+    }
+}
