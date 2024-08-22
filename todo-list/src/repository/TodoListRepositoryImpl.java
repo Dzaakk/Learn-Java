b421cdfb6c0c414686315c91a47bda89d9985e64
Dzkzzz <dzdzaakk@gmail.com>
2024-08-22 23:52:11 +0700

    repository n service impl

---
+++ todo-list/src/repository/TodoListRepositoryImpl.java
@@ -0,0 +1,26 @@
+package repository;
+
+import entity.Todolist;
+
+public class TodoListRepositoryImpl implements TodoListRepository {
+
+    private Todolist[] data = new Todolist[10];
+
+    @Override
+    public Todolist[] getAll() {
+        return data;
+    }
+
+    @Override
+    public void add(Todolist todolist) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'add'");
+    }
+
+    @Override
+    public void remove(Integer number) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'remove'");
+    }
+
+}
---
+++ todo-list/src/service/TodoListServiceImpl.java
@@ -0,0 +1,43 @@
+package service;
+
+import entity.Todolist;
+import repository.TodoListRepository;
+
+public class TodoListServiceImpl implements TodoListService {
+    private TodoListRepository todoListRepository;
+
+    public void setTodoListRepository(TodoListRepository todoListRepository) {
+        this.todoListRepository = todoListRepository;
+    }
+
+    @Override
+    public void showTodoList() {
+        Todolist[] model = todoListRepository.getAll();
+
+        System.out.println("TODOLIST");
+        for (var i = 0; i < model.length; i++) {
+            var todo = model[i];
+            var no = i + 1;
+
+            if (todo != null) {
+                System.out.println(no + ". " + todo);
+            }
+        }
+    }
+
+    @Override
+    public void addTodoList(String todo) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'addTodoList'");
+    }
+
+    @Override
+    public void removeTodoList(Integer number) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'removeTodoList'");
+    }
+
+    public TodoListRepository getTodoListRepository() {
+        return todoListRepository;
+    }
+}
