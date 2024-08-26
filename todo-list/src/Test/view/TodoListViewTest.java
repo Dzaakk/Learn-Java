b18f1b37287d0daba3ce31228fbba1a4007bda48
Dzkzzz <dzdzaakk@gmail.com>
2024-08-26 23:21:51 +0700

    test view show todolist

---
+++ todo-list/src/Test/view/TodoListViewTest.java
@@ -0,0 +1,25 @@
+package Test.view;
+
+import repository.TodoListRepository;
+import repository.TodoListRepositoryImpl;
+import service.TodoListService;
+import service.TodoListServiceImpl;
+import view.TodoListView;
+
+public class TodoListViewTest {
+    public static void main(String[] args) {
+        testShowTodoList();
+    }
+
+    public static void testShowTodoList() {
+        TodoListRepository todoListRepository = new TodoListRepositoryImpl();
+        TodoListService todoListService = new TodoListServiceImpl(todoListRepository);
+        TodoListView todoListView = new TodoListView(todoListService);
+
+        todoListService.addTodoList("Prepare Meal");
+        todoListService.addTodoList("Shower");
+        todoListService.addTodoList("Take a Break");
+
+        todoListView.showTodoList();
+    }
+}
