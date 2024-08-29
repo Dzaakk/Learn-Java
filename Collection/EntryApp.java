9dabb21b821d3b72feb0d88b21372865ec64250d
Dzkzzz <dzdzaakk@gmail.com>
2024-08-29 23:23:54 +0700

    entry map

---
+++ Collection/EntryApp.java
@@ -0,0 +1,21 @@
+package Collection;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+public class EntryApp {
+    public static void main(String[] args) {
+        Map<String, String> map = new HashMap<>();
+
+        map.put("data1", "Banana");
+        map.put("data2", "Watermelon");
+        map.put("data3", "Mango");
+
+        Set<Map.Entry<String, String>> entries = map.entrySet();
+
+        for (var entry : entries) {
+            System.out.println(entry.getKey() + " : " + entry.getValue());
+        }
+    }
+}
