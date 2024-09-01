ab64f474bde4aaba412b067634f47747e17e9dec
Dzkzzz <dzdzaakk@gmail.com>
2024-09-01 22:40:00 +0700

    dependency

--- Maven/learn-maven/src/main/java/dzaakk/learn/maven/App.java
+++ Maven/learn-maven/src/main/java/dzaakk/learn/maven/App.java
@@ -1,10 +1,17 @@
 package dzaakk.learn.maven;
 
+import com.google.gson.Gson;
+
 /**
  * Hello world!
  */
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Gson gson = new Gson();
+
+        Person person = new Person("Agent S");
+        String json = gson.toJson(person);
+
+        System.out.println(json);
     }
 }
