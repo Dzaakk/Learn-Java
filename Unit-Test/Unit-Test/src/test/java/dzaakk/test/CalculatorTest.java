1469add405abd49a0b3a48cbdd4712ae5907051b
Dzkzzz <dzdzaakk@gmail.com>
2024-09-03 23:27:25 +0700

    before all/each and after all/each

--- Unit-Test/Unit-Test/src/test/java/dzaakk/test/CalculatorTest.java
+++ Unit-Test/Unit-Test/src/test/java/dzaakk/test/CalculatorTest.java
@@ -2,6 +2,10 @@ package dzaakk.test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.Test;
@@ -12,6 +16,26 @@ import dzaakk.test.generator.SimpleDisplayNameGenerator;
 // @DisplayName("Test for Calculator")
 public class CalculatorTest {
     Calculator calculator = new Calculator();
+
+    @BeforeAll
+    public static void beforeAll() {
+        System.out.println("Before all"); // execute just once before all unti test
+    }
+
+    @AfterAll
+    public static void afterAll() {
+        System.out.println("After all"); // execute just once after all unit test
+    }
+
+    @BeforeEach
+    public void setUp() {
+        System.out.println("Before each"); // execute multiple times before every unit test
+    }
+
+    @AfterEach
+    public void tearDown() {
+        System.out.println("After each"); // execute multiple times after every unit test
+    }
 
     @Test
     // @DisplayName("Test success add condition")
