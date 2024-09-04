7a7091c88a3a128e698a23e54233bf6248f7bfc3
Dzkzzz <dzdzaakk@gmail.com>
2024-09-04 23:07:34 +0700

    test lifecycle

--- Unit-Test/Unit-Test/src/test/java/dzaakk/test/OrderedTest.java
+++ Unit-Test/Unit-Test/src/test/java/dzaakk/test/OrderedTest.java
@@ -3,25 +3,32 @@ package dzaakk.test;
 import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
 import org.junit.jupiter.api.TestMethodOrder;
 
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class OrderedTest {
+    private int counter = 0;
+
     @Test
     @Order(1)
     void test3() {
-
+        counter++;
+        System.out.println(counter);
     }
 
     @Test
     @Order(3)
     void test2() {
-
+        counter++;
+        System.out.println(counter);
     }
 
     @Test
     @Order(2)
     void test1() {
-
+        counter++;
+        System.out.println(counter);
     }
 }
