dcde782abd0d98cc5b645e6ac54f2e2f563cb434
Dzkzzz <dzdzaakk@gmail.com>
2024-09-05 22:18:40 +0700

    test with parameter

--- Unit-Test/Unit-Test/src/test/java/dzaakk/test/RandomCalculatorTest.java
+++ Unit-Test/Unit-Test/src/test/java/dzaakk/test/RandomCalculatorTest.java
@@ -1,5 +1,6 @@
 package dzaakk.test;
 
+import java.util.List;
 import java.util.Random;
 
 import org.junit.jupiter.api.Assertions;
@@ -8,6 +9,9 @@ import org.junit.jupiter.api.RepeatedTest;
 import org.junit.jupiter.api.RepetitionInfo;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInfo;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 public class RandomCalculatorTest extends AbstractCalculatorTest {
 
@@ -44,6 +48,30 @@ public class RandomCalculatorTest extends AbstractCalculatorTest {
 
         var result = calculator.add(a, b);
         var expected = a + b;
+
+        Assertions.assertEquals(expected, result);
+    }
+
+    @DisplayName("Test calculator")
+    @ParameterizedTest(name = "{displayName} with parameter {0}")
+    @ValueSource(ints = { 1, 3, 5, 6, 23, 45, 67 })
+    void testWithParameter(int value) {
+        var expected = value + value;
+        var result = calculator.add(value, value);
+
+        Assertions.assertEquals(expected, result);
+    }
+
+    public static List<Integer> parameterSource() {
+        return List.of(1, 2, 3, 4, 5, 6, 8, 9);
+    }
+
+    @DisplayName("Test calculator")
+    @ParameterizedTest(name = "{displayName} with parameter {0}")
+    @MethodSource({ "parameterSource" })
+    void testWithMethodSource(Integer value) {
+        var expected = value + value;
+        var result = calculator.add(value, value);
 
         Assertions.assertEquals(expected, result);
     }
