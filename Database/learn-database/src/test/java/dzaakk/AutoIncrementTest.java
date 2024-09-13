b3703572567a7143f46b87eb7de957435549ebba
Dzkzzz <dzdzaakk@gmail.com>
2024-09-13 23:45:53 +0700

    auto increment

---
+++ Database/learn-database/src/test/java/dzaakk/AutoIncrementTest.java
@@ -0,0 +1,31 @@
+package dzaakk;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import org.junit.jupiter.api.Test;
+
+public class AutoIncrementTest {
+    @Test
+    void testAutoIncrement() throws SQLException {
+        Connection connection = ConnectionUtil.getDataSource().getConnection();
+        String sql = "INSERT INTO comments(email, comment) VALUES (?, ?)";
+        PreparedStatement preparedStatement = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+
+        preparedStatement.setString(1, "test3@test.com");
+        preparedStatement.setString(2, "greetings");
+
+        preparedStatement.executeUpdate();
+
+        ResultSet resultSet = preparedStatement.getGeneratedKeys();
+        if (resultSet.next()) {
+            System.out.println("Id Comment : " + resultSet.getInt(1));
+        }
+
+        preparedStatement.close();
+        connection.close();
+    }
+}
