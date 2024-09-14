310bdcea1decafbb3d15fc59a207245be26c197a
Dzkzzz <dzdzaakk@gmail.com>
2024-09-14 22:50:13 +0700

    transaction commit and rollback

--- Database/learn-database/src/test/java/dzaakk/BatchTest.java
+++ Database/learn-database/src/test/java/dzaakk/BatchTest.java
@@ -43,4 +43,42 @@ public class BatchTest {
         preparedStatement.close();
         connection.close();
     }
+
+    @Test
+    void testCommit() throws SQLException {
+        Connection connection = ConnectionUtil.getDataSource().getConnection();
+        connection.setAutoCommit(false);
+        String sql = "INSERT INTO comments(email, comment) VALUES (?, ?)";
+        for (int i = 0; i < 100; i++) {
+            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+            preparedStatement.clearParameters();
+            preparedStatement.setString(1, "test2@test.com");
+            preparedStatement.setString(2, "hello");
+            preparedStatement.executeUpdate();
+            preparedStatement.close();
+        }
+
+        connection.commit();
+        connection.close();
+
+    }
+
+    @Test
+    void testRollback() throws SQLException {
+        Connection connection = ConnectionUtil.getDataSource().getConnection();
+        connection.setAutoCommit(false);
+        String sql = "INSERT INTO comments(email, comment) VALUES (?, ?)";
+        for (int i = 0; i < 100; i++) {
+            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+            preparedStatement.clearParameters();
+            preparedStatement.setString(1, "test2@test.com");
+            preparedStatement.setString(2, "hello");
+            preparedStatement.executeUpdate();
+            preparedStatement.close();
+        }
+
+        connection.rollback();
+        connection.close();
+
+    }
 }
