d4d89431dc44298863924bf85d949f7f804b5e4d
Dzkzzz <dzdzaakk@gmail.com>
2024-09-17 22:36:19 +0700

    initial commit i18n, locale

---
+++ Internationalization/src/test/java/dzaakk/LocaleTest.java
@@ -0,0 +1,33 @@
+package dzaakk;
+
+import java.util.Locale;
+
+import org.junit.jupiter.api.Test;
+
+public class LocaleTest {
+    @Test
+    void testNewLocale() {
+        var language = "in";
+        var country = "ID";
+        var locale = new Locale(language, country);
+
+        System.out.println(locale.getLanguage());
+        System.out.println(locale.getCountry());
+
+        System.out.println(locale.getDisplayLanguage());
+        System.out.println(locale.getDisplayCountry());
+    }
+
+    @Test
+    void testNewLocaleUs() {
+        var language = "en";
+        var country = "US";
+        var locale = new Locale(language, country);
+
+        System.out.println(locale.getLanguage());
+        System.out.println(locale.getCountry());
+
+        System.out.println(locale.getDisplayLanguage());
+        System.out.println(locale.getDisplayCountry());
+    }
+}
