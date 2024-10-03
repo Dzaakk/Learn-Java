461f090ebd035db6ffbbbfa670db86f4b494e5e5
Dzkzzz <dzdzaakk@gmail.com>
2024-10-03 19:32:59 +0700

    deadlock

---
+++ learn-thread/src/main/java/dzaakk/thread/Balance.java
@@ -0,0 +1,28 @@
+package dzaakk.thread;
+
+public class Balance {
+    private Long value;
+
+    public Balance(Long value) {
+        this.value = value;
+    }
+
+    public Long getValue() {
+        return value;
+    }
+
+    public void setValue(Long value) {
+        this.value = value;
+    }
+
+    public static void transfer(Balance from, Balance to, Long value) throws InterruptedException {
+        synchronized (from) {
+            Thread.sleep(1000);
+            from.setValue(from.getValue() - value);
+        }
+        synchronized (to) {
+            Thread.sleep(1000);
+            to.setValue(to.getValue() + value);
+        }
+    }
+}
