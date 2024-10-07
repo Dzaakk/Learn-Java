fda9791cc75a59a23bcb30706ee7672e7c33f12c
Dzkzzz <dzdzaakk@gmail.com>
2024-10-07 17:11:33 +0700

    lock

---
+++ learn-thread/src/main/java/dzaakk/thread/CounterReadWriteLock.java
@@ -0,0 +1,31 @@
+package dzaakk.thread;
+
+import java.util.concurrent.locks.ReadWriteLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+
+public class CounterReadWriteLock {
+    private Long value = 0L;
+
+    final private ReadWriteLock lock = new ReentrantReadWriteLock();
+
+    public void increment() {
+        try {
+            lock.writeLock().lock();
+            ;
+            value++;
+        } finally {
+            lock.writeLock().lock();
+            ;
+        }
+    }
+
+    public Long getValue() {
+        try {
+            lock.readLock().lock();
+            return value;
+        } finally {
+            lock.readLock().unlock();
+        }
+    }
+
+}
